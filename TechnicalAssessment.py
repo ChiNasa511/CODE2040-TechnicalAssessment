e05d0e5232c92ece1ca6a61dc5cde84756874570
Chinasa T. Okolo <theonasa511@hotmail.com>
2016-09-10 15:44:31 -0700

    Update TechnicalAssessment.py

--- TechnicalAssessment.py
+++ TechnicalAssessment.py
@@ -5,13 +5,13 @@
 import json, requests, math, time, datetime
 
 # My API token
-token = '887f59644a4b65b0f9a3cf52ba293ce3'
+APIToken = '887f59644a4b65b0f9a3cf52ba293ce3'
 
 # Step 1 - Connect to the registration endpoint
 def Step1Registration()
   endpoint = 'http://challenge.code2040.org/api/register'
-  github = 'https://github.com/ChiNasa511/CODE2040-TechnicalAssessment'
-  response = requests.post(endpoint, data = {'token': token, 'github': github});
+  myGithub = 'https://github.com/ChiNasa511/CODE2040-TechnicalAssessment'
+  response = requests.post(endpoint, data = {'token': APIToken, 'github': myGithub});
 
   # Print and return the content of the response
   print(response.text)
