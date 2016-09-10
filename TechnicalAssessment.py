e45eb21bc06f49aeb75d9830d64400037015fcd6
Chinasa T. Okolo <theonasa511@hotmail.com>
2016-09-10 15:16:28 -0700

    Update TechnicalAssessment.py

--- TechnicalAssessment.py
+++ TechnicalAssessment.py
@@ -1,4 +1,10 @@
+# Code2040 Fellows Application
+# Technical Assessment
+# Chinasa T. Okolo, Fall 2016
+
+import 
+
 response = requests.get("http://challenge.code2040.org/api/register.json")
-token = 887f59644a4b65b0f9a3cf52ba293ce3
-github = https://github.com/ChiNasa511/CODE2040-TechnicalAssessment
+token = "887f59644a4b65b0f9a3cf52ba293ce3"
+github = "https://github.com/ChiNasa511/CODE2040-TechnicalAssessment"
 print(response.status_code)
