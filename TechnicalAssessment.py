2b99c221314ddd0c3a80019b92f9addacba4dc43
Chinasa T. Okolo <theonasa511@hotmail.com>
2016-09-10 15:13:35 -0700

    Create TechnicalAssessment.py

---
+++ TechnicalAssessment.py
@@ -0,0 +1,4 @@
+response = requests.get("http://challenge.code2040.org/api/register.json")
+token = 887f59644a4b65b0f9a3cf52ba293ce3
+github = https://github.com/ChiNasa511/CODE2040-TechnicalAssessment
+print(response.status_code)
