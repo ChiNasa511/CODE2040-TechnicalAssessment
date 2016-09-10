59a95c99dec6996bb1904041030eb7e992de35d0
Chinasa T. Okolo <theonasa511@hotmail.com>
2016-09-10 16:09:25 -0700

    Update TechnicalAssessment.py

--- TechnicalAssessment.py
+++ TechnicalAssessment.py
@@ -11,11 +11,11 @@ myAPIToken = '887f59644a4b65b0f9a3cf52ba293ce3'
 def Step1Registration():
     myEndpoint = 'http://challenge.code2040.org/api/register'
     myGithub = 'https://github.com/ChiNasa511/CODE2040-TechnicalAssessment'
-    r = requests.post(myEndpoint, data = {'token': myAPIToken, 'github': myGithub})
+    response = requests.post(myEndpoint, data = {'token': myAPIToken, 'github': myGithub})
 
     # Print and return the content of the response
-    print(r.text)
-    return(r.text)
+    print(response.text)
+    return(response.text)
     
     
 
