e92ba203ab823e9b66f2a75d4d1b0974434b09f2
Chinasa T. Okolo <theonasa511@hotmail.com>
2016-09-18 01:11:55 -0500

    Update TechnicalAssessment.py

--- TechnicalAssessment.py
+++ TechnicalAssessment.py
@@ -14,18 +14,13 @@ TOKEN = '887f59644a4b65b0f9a3cf52ba293ce3'
 
 # Step 1 - Connect to the registration endpoint
 def Step1Registration(): 
-   # myEndpoint = 'http://challenge.code2040.org/api/register'
+    myEndpoint = 'http://challenge.code2040.org/api/register'
     myGitHub = 'https://github.com/ChiNasa511/CODE2040-TechnicalAssessment/'
-    #myKeys = {'token': TOKEN, 'github': myGithub}
-    #response = requests.post(myEndpoint, json = myKeys)
+    myKeys = {'token': TOKEN, 'github': myGitHub}
+    response = requests.post(myEndpoint, json = myKeys)
 
     # Print the content of the response
-    #print(response.text)
-    
-     registration_data = {'token': TOKEN, 'github': myGitHub}
-     req = urllib2.Request('http://challenge.code2040.org/api/register')
-     req.add_header('Content-Type', 'application/json')
-     response = urllib2.urlopen(req, json.dumps(registration_data))
+    print(response.text)
 
 # Step 2 - Connect to the registration endpoint
 def Step2():
