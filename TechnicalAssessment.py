f3183362e1839e5d4b33b812a283586a8227a018
Chinasa T. Okolo <theonasa511@hotmail.com>
2016-09-19 02:05:36 -0700

    Update TechnicalAssessment.py

--- TechnicalAssessment.py
+++ TechnicalAssessment.py
@@ -15,10 +15,10 @@ def Step1Registration():
     response = requests.post(myEndpoint, myKeys)
     
     # Check for accuracy (was trying to do unit tests but not working)
-    if response.status_code != 200:
-        print 'Registration Complete'
-    else:
-        print 'Please Try Again'
+   # if response.status_code != 200:
+    #    print 'Registration Complete'
+    #else:
+     #   print 'Please Try Again'
 
     # Print the content of the response
     print response.text
